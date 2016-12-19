4a1980d431c40afe2f4929405981afb8c7345e1b
harsh306 <harshnpathak@gmail.com>
2016-12-19 13:13:13 +0530

    log of tuples added in operators

--- spark-example/src/main/java/com/datatorrent/example/utils/MapOperator.java
+++ spark-example/src/main/java/com/datatorrent/example/utils/MapOperator.java
@@ -29,10 +29,10 @@ public class MapOperator<T,U> extends MyBaseOperator implements Serializable {
         public void process(T tuple) {
                 try {
                     output.emit((U) f.apply(tuple));
-                    log.info("Function applied on tuple {} of OperatorID {} at Map",tuple,ID);
+                    log.info("Function will be applied on tuple {} of OperatorID {} in Map",tuple,ID);
                 }
                 catch (Exception e){
-                    log.info("Exception Occured Due to {} of OperatorID {} at Map",tuple,ID);
+                    log.info("Exception Occured Due to {} of OperatorID {} in Map",tuple,ID);
                     output.emit((U)tuple);
                     e.printStackTrace();
                 }
