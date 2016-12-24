9d14bc7d38ba41b128b81fb983684a212d0c7eb6
harsh306 <harshnpathak@gmail.com>
2016-12-24 22:35:56 +0530

    iterator serialization in MapPartitionOperator

--- spark-example/src/main/java/com/datatorrent/example/utils/MapPartitionOperator.java
+++ spark-example/src/main/java/com/datatorrent/example/utils/MapPartitionOperator.java
@@ -2,18 +2,17 @@ package com.datatorrent.example.utils;
 
 import com.datatorrent.api.Context;
 import com.esotericsoftware.kryo.DefaultSerializer;
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
 import com.esotericsoftware.kryo.serializers.JavaSerializer;
 import org.apache.commons.collections.iterators.ArrayListIterator;
 import org.apache.spark.TaskContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Function3;
-import scala.collection.Iterable;
 import scala.collection.Iterator;
 import scala.collection.JavaConversions$;
-import scala.collection.JavaConverters$;
+
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ListIterator;
 
 @DefaultSerializer(JavaSerializer.class)
@@ -25,6 +24,7 @@ public class MapPartitionOperator<T,U> extends MyBaseOperator implements Seriali
     Logger log = LoggerFactory.getLogger(MapPartitionOperator.class);
     public Function3<TaskContext, Object, Iterator<T>, Iterator<U>> f;
     public DefaultOutputPortSerializable output = new DefaultOutputPortSerializable();
+    public Iterator<T> iterT;
 
     @Override
     public void setup(Context.OperatorContext context) {
@@ -34,13 +34,14 @@ public class MapPartitionOperator<T,U> extends MyBaseOperator implements Seriali
 
     public DefaultInputPortSerializable<Object> input = new DefaultInputPortSerializable<Object>() {
         @Override
-        public void process(final Object tuple) {
+        public void process(Object tuple) {
             try {
                 String[] a = tuple.toString().split(",");
                 ListIterator list = new ArrayListIterator(a);
-                Iterator iter1 =JavaConversions$.MODULE$.asScalaIterator(list);
-
-                output.emit(f.apply(taskContext,object, iter1));
+                iterT =JavaConversions$.MODULE$.asScalaIterator(list);
+                Iterator<U> iterU = f.apply(taskContext,object, iterT);
+                output.emit(iterU);
+                log.info(String.valueOf((f.apply(taskContext,object, iterT)).getClass()));
             } catch (Exception e){
                 log.info("Exception Occured Due to {}  {} of OperatorID {} in MapPartition",tuple,tuple.getClass(),ID);
                 e.printStackTrace();
